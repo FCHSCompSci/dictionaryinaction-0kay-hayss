cd7b5208582f6c405749e5bee0ef8878fa2a0ea9
Hayley Renae Kustes <hayley.kustes@stu.jefferson.kyschools.us>
2018-09-18 09:36:40 -0400

    added this feature

--- dictionary project.py
+++ dictionary project.py
@@ -1,5 +1,13 @@
 harry_potter_game = {
     'display_name': '0kay.hayss',
     'house': 'gryfindor',
-    '
+    'xp_in_game': 1000,
+    'year_in_game': 1,
+    }
+while True:
+    new_name = input("What's your new display name?: ")
+    
+    
+    
+    
     
