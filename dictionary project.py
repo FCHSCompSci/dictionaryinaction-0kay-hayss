2ee08fd1cfb2d0caa5157e453b51c73370383d3d
Hayley Renae Kustes <hayley.kustes@stu.jefferson.kyschools.us>
2018-09-20 09:35:46 -0400

    feature added

--- dictionary project.py
+++ dictionary project.py
@@ -1,11 +1,19 @@
-harry_potter_game = {
+hp_profile = {
     'display_name': '0kay.hayss',
     'house': 'gryfindor',
     'xp_in_game': 1000,
     'year_in_game': 1,
     }
+
 while True:
-    new_name = input("What's your new display name?: ")
+    question = input("Do you want to change the username? Press stop to quit: ")
+    if question == "stop":
+        break
+    elif =
+        new_name = input("What's your new username?: ")
+        hp_profile['display_name'] = new_name
+
+    
     
     
     
